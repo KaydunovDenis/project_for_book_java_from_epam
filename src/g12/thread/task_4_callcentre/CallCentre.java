c71081eed155bd93465184765620600219097c6b
Denis Kaydunov <deniskaydunov@sap.com>
2024-02-19 11:20:12 +0300

    has been added task 12-4 call centre

---
+++ src/g12/thread/task_4_callcentre/CallCentre.java
@@ -0,0 +1,13 @@
+package g12.thread.task_4_callcentre;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CallCentre {
+
+    List<Operator> operators = new ArrayList<Operator>();
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/g12/thread/task_4_callcentre/Operator.java
@@ -0,0 +1,4 @@
+package g12.thread.task_4_callcentre;
+
+public class Operator {
+}
